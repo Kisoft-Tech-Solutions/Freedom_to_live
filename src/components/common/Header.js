c2191490e2f5fb82290fb5ac5a440e310bff209b
sameer-kharel <shameerkharel@gmail.com>
2025-07-15 11:48:31 +0545

    fixed header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from 'next/link';
-import Image from 'next/image'; // Add this import
+import Image from 'next/image'; 
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
